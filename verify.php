eb07c95992f34135bfd66e98dbcd9c164346b516
wtmsys <wtmsystems@gmail.com>
2019-11-24 16:10:19 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,15 +1,10 @@
-<?php
-$access_token = 'sdQDgB7nnIUBdH6pRuIuOthhuwlPARCoA5rlssPf4qyUd6RGOTTlxuWKUIz2U5z/u1Xv5JDNxl85mjeTcwpWP684FqvUak5WEgQ2LhmwbvHLeTPzdRAprzcsCsmofXSuQOPFzeNneKlt4Pgh1N4w1AdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
-
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+<?php 
+$access_token = 'LY8xrTbp4yIEqtm5ocU/cogLXF7Vm6jWAfqqVp8DHDRyGPeqgDRjUWKkfGzENpGXKE3YBM74JKkmBTlTPy/3ZPcYRKMdF1cN2PhXgC5sANhkVGNUysbi1A4NdwnZTHa5PufzPVZapPhJVKBOu/SargdB04t89/1O/w1cDnyilFU=';
+  $url = 'https://api.line.me/v1/oauth/verify';
+  $headers = array('Authorization: Bearer ' . $access_token);
+  $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);curl_close($ch);
+  echo $result;
